00f94d9cac284374f6bb4f1a8e7e0b89aff2ba7f
Thomas Ip <thomas@ipthomas.com>
2020-11-22 19:27:59 +0800

    Handle lexing of indentation correctly

--- src/lexer.ml
+++ src/lexer.ml
@@ -12,76 +12,75 @@ let uppercase = [%sedlex.regexp? 'A'..'Z']
 let ident = [%sedlex.regexp? (lowercase | uppercase | '_'), Star (lowercase | uppercase | digit | '_')]
 
 type tokenizer_state =
-  { mutable is_newline: bool;
-    mutable indent_stack: int CCList.t;
-    mutable dedents_to_emit: int;
+  { mutable indent_stack: int CCList.t;
+    deferred_tokens: Tokens.t CCDeque.t;
   }
 
 let rec tokenize state lexbuf =
-  if state.dedents_to_emit > 0 then begin
-    state.dedents_to_emit <- state.dedents_to_emit - 1;
-    DEDENT
-  end else match%sedlex lexbuf with
-  | Plus ' ' ->
-      if Sedlexing.lexeme_start lexbuf > 0 then
-        tokenize state lexbuf
-      else
-        raise (LexingError ("Unexpected indent at the start of file", Sedlexing.lexing_positions lexbuf))
-  | '(' -> L_PAREN
-  | ')' -> R_PAREN
-  | '{' -> L_BRACKET
-  | '}' -> R_BRACKET
-  | ',' -> COMMA
-  | ':' -> COLON
-  | '+' -> PLUS
-  | '-' -> MINUS
-  | '*' -> TIMES
-  | '/' -> SLASH
-  | "()" -> UNIT
-  | "true" -> BOOL true
-  | "false" -> BOOL false
-  | integer -> INT (Int64.of_string (Sedlexing.Utf8.lexeme lexbuf))
-  | float -> FLOAT (Float.of_string (Sedlexing.Utf8.lexeme lexbuf))
-  | '\'', any, Star Sub(any, '\''),'\'' ->
-      (* A char is a unicode extended grapheme cluster which can contain
-         multiple codepoints. *)
-      let n_codepoints = Sedlexing.lexeme_length lexbuf - 2 in
-      let c = Sedlexing.Utf8.sub_lexeme lexbuf 1 n_codepoints in
-      if utf8_len c = 1 then CHAR c
-      else raise (LexingError (
-        "This character literal contains more than one grapheme",
-        Sedlexing.lexing_positions lexbuf))
-  | '"', Star Sub(any, '"'), '"' ->
-      let len = Sedlexing.lexeme_length lexbuf - 2 in
-      STRING (Sedlexing.Utf8.sub_lexeme lexbuf 1 len)
-  | '\n', Star ' ' ->
-      let open CCList in
-      let curr_indent = Sedlexing.lexeme_length lexbuf - 1 in
-      let prev_indent = hd state.indent_stack in
-      if curr_indent > prev_indent then begin
-        state.indent_stack <- curr_indent :: state.indent_stack;
-        INDENT
-      end else if curr_indent < prev_indent then begin
-        (* dedents *)
-        let prev_depth = length state.indent_stack in
-        state.indent_stack <- drop_while ((<) curr_indent) state.indent_stack;
-        if hd state.indent_stack = curr_indent then begin
-          state.dedents_to_emit <- prev_depth - length state.indent_stack;
+  match CCDeque.take_front_opt state.deferred_tokens with
+  | Some tok -> tok
+  | None -> begin
+    match%sedlex lexbuf with
+    | Plus ' ' ->
+        if Sedlexing.lexeme_start lexbuf > 0 then
           tokenize state lexbuf
-        end else
-          raise (LexingError ("Unexpected indent size", Sedlexing.lexing_positions lexbuf))
-      end else
-        (* same indentation *)
-        tokenize state lexbuf
-  | eof -> EOF
-  | _ -> failwith "Unknown token"
+        else
+          raise (LexingError ("Unexpected indent at the start of file", Sedlexing.lexing_positions lexbuf))
+    | '(' -> L_PAREN
+    | ')' -> R_PAREN
+    | '{' -> L_BRACKET
+    | '}' -> R_BRACKET
+    | ',' -> COMMA
+    | ':' -> COLON
+    | '+' -> PLUS
+    | '-' -> MINUS
+    | '*' -> TIMES
+    | '/' -> SLASH
+    | "()" -> UNIT
+    | "true" -> BOOL true
+    | "false" -> BOOL false
+    | integer -> INT (Int64.of_string (Sedlexing.Utf8.lexeme lexbuf))
+    | float -> FLOAT (Float.of_string (Sedlexing.Utf8.lexeme lexbuf))
+    | '\'', any, Star Sub(any, '\''),'\'' ->
+        (* A char is a unicode extended grapheme cluster which can contain
+           multiple codepoints. *)
+        let n_codepoints = Sedlexing.lexeme_length lexbuf - 2 in
+        let c = Sedlexing.Utf8.sub_lexeme lexbuf 1 n_codepoints in
+        if utf8_len c = 1 then CHAR c
+        else raise (LexingError (
+          "This character literal contains more than one grapheme",
+          Sedlexing.lexing_positions lexbuf))
+    | '"', Star Sub(any, '"'), '"' ->
+        let len = Sedlexing.lexeme_length lexbuf - 2 in
+        STRING (Sedlexing.Utf8.sub_lexeme lexbuf 1 len)
+    | ident -> IDENT (Sedlexing.Utf8.lexeme lexbuf)
+    | '\n', Star ' ' ->
+        let curr_indent = Sedlexing.lexeme_length lexbuf - 1 in
+        let rec aux dedenting = function
+          | prev_indent :: tl when curr_indent > prev_indent ->
+            if not dedenting then begin
+                CCDeque.push_back state.deferred_tokens INDENT;
+                curr_indent :: prev_indent :: tl
+            end else
+              raise (LexingError ("Unexpected indentation", Sedlexing.lexing_positions lexbuf))
+          | prev_indent :: tl when curr_indent < prev_indent ->
+            begin
+              CCDeque.push_back state.deferred_tokens DEDENT;
+              aux true tl
+            end
+          | stack (* same indentation *) -> stack
+        in
+        state.indent_stack <- aux false state.indent_stack;
+        NEWLINE
+    | eof -> EOF
+    | _ -> failwith "Unknown token"
+  end
 
 (* Capatibility with Menhir revised API *)
 let init lexbuf =
   let state = {
-    is_newline=true;
     indent_stack=[0];
-    dedents_to_emit=0;
+    deferred_tokens=CCDeque.create ();
   } in
   fun () ->
     let tok = tokenize state lexbuf in
